c9ce5cfa131a36c3ed6da6b5c221486e4901a17b
key <a000058556@gmail.com>
2022-09-23 14:39:38 +0800

    set & SimplifiedSettingsController

--- mvc/app/controllers/OPNsense/HelloWorld/Api/SettingsController.php
+++ mvc/app/controllers/OPNsense/HelloWorld/Api/SettingsController.php
@@ -3,10 +3,12 @@ namespace OPNsense\HelloWorld\Api;
 
 use \OPNsense\Base\ApiControllerBase;
 use \OPNsense\HelloWorld\HelloWorld;
+use \OPNsense\Core\Config;
 
 
 class SettingsController extends ApiControllerBase
 {
+    // 設定get取資料
     public function getAction()
     {
         // define list of configurable settings
@@ -14,6 +16,34 @@ class SettingsController extends ApiControllerBase
         if ($this->request->isGet()) {
             $mdlHelloWorld = new HelloWorld();
             $result['helloworld'] = $mdlHelloWorld->getNodes();
+        }
+        return $result;
+    }
+
+    // 設定set保存資料
+    public function setAction()
+    {
+        $result = array("result"=>"failed");
+        if ($this->request->isPost()) {
+            // load model and update with provided data
+            $mdlHelloWorld = new HelloWorld();
+            $mdlHelloWorld->setNodes($this->request->getPost("helloworld"));
+
+            // perform validation
+            $valMsgs = $mdlHelloWorld->performValidation();
+            foreach ($valMsgs as $field => $msg) {
+                if (!array_key_exists("validations", $result)) {
+                    $result["validations"] = array();
+                }
+                $result["validations"]["general.".$msg->getField()] = $msg->getMessage();
+            }
+
+            // serialize model to config and save
+            if ($valMsgs->count() == 0) {
+                $mdlHelloWorld->serializeToConfig();
+                Config::getInstance()->save();
+                $result["result"] = "saved";
+            }
         }
         return $result;
     }
---
+++ mvc/app/controllers/OPNsense/HelloWorld/Api/SimplifiedSettingsController.php
@@ -0,0 +1,7 @@
+<?php
+
+class SimplifiedSettingsController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelName = 'helloworld';
+    protected static $internalModelClass = 'OPNsense\HelloWorld\HelloWorld';
+}
