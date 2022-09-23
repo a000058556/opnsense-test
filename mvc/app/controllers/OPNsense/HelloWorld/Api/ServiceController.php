bf7791f947322c230f342dc22afe76250fd27feb
key <a000058556@gmail.com>
2022-09-23 15:39:54 +0800

    save helloworld.conf

--- mvc/app/controllers/OPNsense/HelloWorld/Api/ServiceController.php
+++ mvc/app/controllers/OPNsense/HelloWorld/Api/ServiceController.php
@@ -1,7 +1,20 @@
 <?php
 namespace OPNsense\HelloWorld\Api;
+use \OPNsense\Core\Backend;
 
 use \OPNsense\Base\ApiControllerBase;
 class ServiceController extends ApiControllerBase
 {
+    public function reloadAction()
+    {
+        $status = "failed";
+        if ($this->request->isPost()) {
+            $backend = new Backend();
+            $bckresult = trim($backend->configdRun("template reload OPNsense/HelloWorld"));
+            if ($bckresult == "OK") {
+                $status = "ok";
+            }
+        }
+        return array("status" => $status);
+    }
 }
