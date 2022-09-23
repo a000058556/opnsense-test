03b0990a24788532fd4716e1074d4bc05ce753a3
key <a000058556@gmail.com>
2022-09-23 14:28:13 +0800

    SettingsController api get

--- mvc/app/controllers/OPNsense/HelloWorld/Api/SettingsController.php
+++ mvc/app/controllers/OPNsense/HelloWorld/Api/SettingsController.php
@@ -2,6 +2,19 @@
 namespace OPNsense\HelloWorld\Api;
 
 use \OPNsense\Base\ApiControllerBase;
+use \OPNsense\HelloWorld\HelloWorld;
+
+
 class SettingsController extends ApiControllerBase
 {
+    public function getAction()
+    {
+        // define list of configurable settings
+        $result = array();
+        if ($this->request->isGet()) {
+            $mdlHelloWorld = new HelloWorld();
+            $result['helloworld'] = $mdlHelloWorld->getNodes();
+        }
+        return $result;
+    }
 }
--- mvc/app/views/OPNsense/HelloWorld/index.volt
+++ mvc/app/views/OPNsense/HelloWorld/index.volt
@@ -1,1 +1,2 @@
+<!-- 使用 generalForm 的內容添以base_form.volt為基底建立一個表單，並在 HTML 中命名為 frm_GeneralSettings。 -->
 {{ partial("layout_partials/base_form",['fields':generalForm,'id':'frm_GeneralSettings'])}}
