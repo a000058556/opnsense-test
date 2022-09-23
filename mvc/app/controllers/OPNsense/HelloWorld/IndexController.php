3ff1efee6090bca2deef1e264865d07f4ac21d45
key <a000058556@gmail.com>
2022-09-23 11:26:44 +0800

    -4

--- mvc/app/controllers/OPNsense/HelloWorld/IndexController.php
+++ mvc/app/controllers/OPNsense/HelloWorld/IndexController.php
@@ -5,7 +5,7 @@ class IndexController extends \OPNsense\Base\IndexController
     public function indexAction()
     {
         // pick the template to serve to our users.
-        $this->view->pick('OPNsense/HelloWorld/index');
+        // $this->view->pick('OPNsense/HelloWorld/index');
         $this->view->generalForm = $this->getForm("general");
     }
 }
